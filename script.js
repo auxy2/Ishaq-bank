838e6d4b051156ec33ccbbd006deae2535f69299
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-11-01 12:25:31 +0100

    Updated

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 /////////////////////////////////////////////////
 // Data
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: "isihaq abdullahi",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
